38e717fd68eb3ca63edc2f0b6fbf49929a051c98
sumitkau <sumit.kau@gmail.com>
2017-06-29 17:29:41 +0530

    kubernetes certs

---
+++ roles/certs/files/gen-kubernetes-srv-cert-key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/k8s-v1.7/certs/;cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
