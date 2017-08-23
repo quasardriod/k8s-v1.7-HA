db9a92c9fb4d21ebbfe9de2e29b09b3d19a0bbc7
sumit <sumit.kau@gmail.com>
2017-08-24 00:22:04 +0530

    certs

--- roles/1-create-certs/files/gen-kube_proxy-client-cert-key.sh
+++ roles/1-create-certs/files/gen-kube_proxy-client-cert-key.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cd /k8s-v1.7/certs/; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kube-proxy-csr.json | cfssljson -bare kube-proxy
+cd /k8s-v1.7/certs/;
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-proxy-csr.json | cfssljson -bare kube-proxy
