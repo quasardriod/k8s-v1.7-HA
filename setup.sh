0c5fd8f7b5b6df21b98deb7d18c98cbbfa11ffe0
sumitkau <sumit.kau@gmail.com>
2017-08-24 09:42:20 +0530

    certs done

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "local_user: $USER" > local_user.yaml
-ansible-playbook -i user-setup-inventory.yaml user-setup.yaml
-rm -rf local_user.yaml
+#echo "local_user: $USER" > local_user.yaml
+#ansible-playbook -i user-setup-inventory.yaml user-setup.yaml
+#rm -rf local_user.yaml
 ansible-playbook -i k8s-inventory.yaml deploy.yaml
